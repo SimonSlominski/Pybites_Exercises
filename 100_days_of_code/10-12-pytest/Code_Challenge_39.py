fcb5495f48da7fb076ea76ed86bd41f444cbd2cd
Szymon Słomiński <simon.slominski@gmail.com>
2020-03-10 09:34:02 +0100

    Add Code_Challenge_39

---
+++ 100_days_of_code/10-12-pytest/Code_Challenge_39.py
@@ -0,0 +1,13 @@
+"""
+Code Challenge 39 - Writing Tests With Pytest
+
+Add tests for one of your repos that lacks tests. You can use BetterCodeHub to see how badly a project needs tests.
+"""
+
+def is_even(number):
+	return number % 2 == 0
+
+
+def sum(num1, num2):
+	"""It returns sum of two numbers"""
+	return num1 + num2
