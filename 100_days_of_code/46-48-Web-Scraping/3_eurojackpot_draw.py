fa661dc966608d7779b50c67268e41f669a9272f
Szymon Słomiński <simon.slominski@gmail.com>
2020-11-16 14:06:27 +0100

    Add eurojackpot draw algorithm

---
+++ 100_days_of_code/46-48-Web-Scraping/3_eurojackpot_draw.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import random
+import os
+
+
+# Get the full path of the current directory
+PATH = os.path.dirname(__file__)
+
+# Load data, change column name, delete duplicated column
+euro_nums_occurrence = pd.read_csv(PATH + '/euro_nums_occurrence.csv', index_col=0)
+euro_nums_occurrence['x times'] = euro_nums_occurrence['Euro']
+euro_nums_occurrence = euro_nums_occurrence.drop('Euro', 1)
+
+def get_main_numbers():
+    for pool, numbers in (50, 5),:
+        main_numbers = sorted(random.sample(range(1, pool + 1), numbers))
+    return main_numbers
+
+def get_euro_numbers():
+    for pool, numbers in (10, 2), :
+        euro_numbers = sorted(random.sample(range(1, pool + 1), numbers))
+    return euro_numbers
+
+def get_eurojackpot_numbers(number_of_draws):
+    eurojackpot_nums = [(get_main_numbers(), get_euro_numbers())
+                        for draw in range(int(number_of_draws))]
+    print("The next Eurojackpot numbers may be: ")
+    return eurojackpot_nums
+
+def get_euro_nums_most_common(grater_then):
+    # grater_then is the number of occurrence
+    return euro_nums_occurrence[euro_nums_occurrence['x times'] > grater_then]
+
+
+if __name__ == "__main__":
+    print(get_eurojackpot_numbers(7))
+
+    print(f"Most common euro numbers are:")
+    print(get_euro_nums_most_common(12))
