e7cedc9ddf943df8897340b72f3c4b12276397d8
Szymon Słomiński <simon.slominski@gmail.com>
2021-06-28 18:09:53 +0200

    Update bite 138

--- Pybites/138/zoo.py
+++ Pybites/138/zoo.py
@@ -52,3 +52,24 @@ if __name__ == '__main__':
     lion = Animal('lion')
     mouse = Animal('mouse')
     print(Animal.zoo())
+
+"""
+The code before cleaning and refactoring 
+
+class Animal():
+    _sequence = 10000
+    animals = []
+
+    def __init__(self, name):
+        self.name = name
+        Animal._sequence += 1
+        self.id = Animal._sequence
+        Animal.animals.append((str(self.id) + ". " + name.title()))
+
+    def __str__(self):
+        return f"{self.id}. {self.name.title()}"
+
+    @classmethod
+    def zoo(cls):
+        return '\n'.join('{}'.format(animal) for animal in cls.animals)
+"""
